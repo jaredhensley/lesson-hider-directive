973572fc35efc9ee2a3471a93bdd7a15f3a21479
Jared Hensley <jahensle85@me.com>
2015-11-12 01:35:12 -0600

    finished

--- lessonCtrl.js
+++ lessonCtrl.js
@@ -1,10 +1,24 @@
 angular.module('directivePractice').controller('mainCtrl', function ($scope) {
+
   $scope.lessons = ['Services', 'Routing', 'Directives', 'Review', 'Firebase', 'No server project', 'Node', 'Express', 'Mongo'];
+
+  $scope.removeLesson = function (name) {
+    for (var i = 0; i < $scope.lessons.length; i++) {
+      if (name === $scope.lessons[i]) {
+        $scope.lessons.splice(i, 1);
+      }
+    }
+
+  }
 
   $scope.test = 'two way data binding bro!';
 
   $scope.announceDay = function (lesson, day) {
-    console.log(lesson + ' is active on ' + day + '.');
-
+    if (!day) {
+      console.log(lesson + ' is not currently scheduled');
+    } else {
+      console.log(lesson + ' is scheduled on ' + day + '.');
+    }
   }
+
 });
--- lessonHider.js
+++ lessonHider.js
@@ -4,31 +4,45 @@ angular.module('directivePractice')
       templateUrl: 'templates/lessonHider.html',
       restrict: 'E',
       scope: {
+        removeLesson: '&',
         lesson: '=',
         dayAlert: '&'
       },
       controller: function ($scope, lessonService) {
         $scope.getSchedule = lessonService.getSchedule();
+        $scope.delete = function (elem) {
+
+        }
       },
-      link: function (scope, element, attributes) {
-        scope.getSchedule.then(function (response) {
-
-          scope.schedule = response.data;
-          console.log(scope.schedule, scope.lesson);
-
-          scope.schedule.forEach(function (value) {
-            if (value.lesson === scope.lesson) {
-              scope.lessonDay = value.weekday;
-
-              element.css('text-decoration', 'line-through');
-              return;
-            }
+      link: function (scope, elem, attr) {
+        // scope.isChecked = true;
 
 
-          });
-
+        scope.toggleChecked = function () {
+          if (scope.isChecked === false) {
+            elem.css('text-decoration', 'none')
+              // scope.isChecked = false;
+          } else {
+            // scope.isChecked = true;
+            elem.css('text-decoration', 'line-through');
+          }
+        }
+        scope.getSchedule.then(function (sched) {
+          scope.schedule = sched.data;
+          var formatData = function (arr, lesson) {
+            for (var i = 0; i < arr.length; i++) {
+              if (arr[i].lesson === lesson) {
+                scope.lessonDay = arr[i].weekday
+                elem.css('text-decoration', 'line-through')
+                scope.isChecked = true;
+                break;
+              } else {
+                scope.isChecked = false;
+              }
+            };
+          };
+          formatData(scope.schedule, scope.lesson)
         });
       }
-
     }
   });
